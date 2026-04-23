cd78d647c21724b379d3804214e30b03a5a93e70
Talha Mahmood <rana.talha20003@gmail.com>
2026-04-23 19:28:30 +0200

    added all the functionality in project just left with css

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -11,21 +11,70 @@ class Game extends Component {
       compScore: 0,
     };
   }
+  logic = (playerVal, computerVal) => {
+    if (playerVal == computerVal) {
+      return 0;
+    } else if (
+      (playerVal == "ROCk" && computerVal == "SCISSORS") ||
+      (playerVal == "SCISSORS" && computerVal == "PAPER") ||
+      (playerVal == "PAPER" && computerVal == "ROCK")
+    ) {
+      return 1;
+    } else {
+      return -1;
+    }
+  };
+  decision = (playerChoice) => {
+    const choices = ["ROCK", "PAPER", "SCISSORS"];
+    const compChoice = choices[Math.floor(Math.random() * choices.length)];
+    const val = this.logic(playerChoice, compChoice);
+    if (val == 1) {
+      console.log("hello");
+      this.setState({
+        playerVal: playerChoice,
+        computerVal: compChoice,
+        playerScore: this.state.playerScore + 1,
+      });
+    } else if (val == -1) {
+      console.log("Hello");
+      this.setState({
+        playerVal: playerChoice,
+        computerVal: compChoice,
+        compScore: this.state.compScore + 1,
+      });
+    } else {
+      console.log("Hello");
+      this.setState({
+        computerVal: compChoice,
+        playerVal: playerChoice,
+      });
+    }
+  };
 
   render() {
+    const { playerVal, computerVal, playerScore, compScore } = this.state;
     return (
-      <div>
+      <div className="container">
         <h1>Welcome to Rock, Paper ,Scissors Game</h1>
         <div>
-          <button>Rock</button>
-          <button>PAPER</button>
-          <button>SCISSOR</button>
+          <button onClick={() => this.decision("ROCK")}>
+            <i className={`fas fa-hand-rock`} />
+            Rock
+          </button>
+          <button onClick={() => this.decision("PAPER")}>
+            <i className={`fas fa-hand-paper`} />
+            PAPER
+          </button>
+          <button onClick={() => this.decision("SCISSORS")}>
+            <i className={`fas fa-hand-scissors`} />
+            SCISSORS
+          </button>
         </div>
-        <div>
-          <p>Your choice: </p>
-          <p>Computer's choice:</p>
-          <h2>Your Score: </h2>
-          <h2>Computer Score: </h2>
+        <div className="content">
+          <p>Your choice:{playerVal} </p>
+          <p>Computer's choice:{computerVal}</p>
+          <h2>Your Score: {playerScore}</h2>
+          <h2>Computer Score: {compScore}</h2>
         </div>
       </div>
     );
